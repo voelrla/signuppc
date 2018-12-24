7bb917c2a925428c7733574977a3857d006aa2cb
Administrator <Administrator@2015080004-3WVP>
2018-12-24 15:20:12 +0900

    alt+tab+enter

--- src/test/java/signupPC/signupPC/setting.java
+++ src/test/java/signupPC/signupPC/setting.java
@@ -41,6 +41,7 @@ public class setting {
 		Robot rb = new Robot();
 		rb.keyPress(KeyEvent.VK_ALT);
 		rb.keyPress(KeyEvent.VK_TAB);
+		rb.keyPress(KeyEvent.VK_ENTER);
 	}
 
 	public static void closetab() throws AWTException {
